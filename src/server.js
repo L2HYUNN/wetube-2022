52bee447e7a8465b4f4b9a70ec3f7e83b92eb9ae
L2HYUNN <dhl9810@naver.com>
2022-01-12 12:00:45 +0900

    #3.0 Your First Server

---
+++ src/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+// const express = require("express");
+
+const PORT = 4000;
+
+const app = express();
+
+const handleListening = () => {
+    console.log(`✅ Server Listening on port http://localhost:${PORT} 🚀`);
+};
+
+app.listen(PORT, handleListening);
