91047bad37c0ae4e4cd32728b4d9847b083db95f
L2HYUNN <dhl9810@naver.com>
2022-01-14 20:08:24 +0900

    #4.2 Cleaning the Code

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 // const express = require("express");
 // #3.1 GET Request
 // #3.4 Recap
@@ -9,36 +12,11 @@ const PORT = 4000;
 const app = express();
 const logger = morgan("dev");
 
-const globalRouter = express.Router();
-const userRouter = express.Router();
-const videoRouter = express.Router();
-
-
-const handleHome = (req, res) => {
-    return res.send("Home");
-} 
-
-globalRouter.get("/", handleHome);
-
-const handleEdit = (req, res) => { 
-    return res.send("Edit");
-}
-
-userRouter.get("/edit", handleEdit);
-
-const handleWatch = (req, res) => {
-    return res.send("Watch");
-}
-
-videoRouter.get("/watch", handleWatch);
-
 app.use(logger);
 
 app.use("/", globalRouter);
 app.use("/users", userRouter);
 app.use("/videos", videoRouter);
-
-app.get("/", handleHome);
 
 
 const handleListening = () => {
