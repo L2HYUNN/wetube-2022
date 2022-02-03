cbf02dc12cc79b20e34e1a0bfa8dba8eed57c4dd
L2HYUNN <dhl9810@naver.com>
2022-02-03 11:47:53 +0900

    #6.8 Connecting to Mongo

---
+++ src/db.js
@@ -0,0 +1,16 @@
+// mongoose 설정하기 
+import mongoose from "mongoose";
+
+const URL = "mongodb://127.0.0.1:27017/wetube";
+
+mongoose.connect(URL, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// mongoose를 통해 mongo connection 알려주기 
+const db = mongoose.connection;
+
+const handleOpen = () => console.log("✅ Connected to DB 🚀");
+const handleError = (error) => console.log("❌ DB Error", error);
+
+db.on("error", handleError);
+db.once("open", handleOpen);
+
