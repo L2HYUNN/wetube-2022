5354d49b2fa371bf6347a9cfe77770b923c7d2ff
L2HYUNN <dhl9810@naver.com>
2022-03-04 19:36:56 +0900

    #9.0 Introduction to Webpack

--- src/init.js
+++ src/init.js
@@ -7,7 +7,9 @@ import app from "./server";
 const PORT = 4000;
 
 const handleListening = () => {
-    console.log(`✅ Server Listening on port http://localhost:${PORT} 🚀`);
+  console.log(`✅ Server Listening on port http://localhost:${PORT} 🚀`);
 };
 
 app.listen(PORT, handleListening);
+
+// + Start Webpack
