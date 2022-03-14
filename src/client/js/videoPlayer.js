77cedae8a440d7a08821255834e9338519702aae
L2HYUNN <dhl9810@naver.com>
2022-03-14 22:40:57 +0900

    #11.1 Play Pause

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,1 +1,29 @@
 console.log("video player");
+
+const video = document.querySelector("video");
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const time = document.getElementById("time");
+const volumeBtn = document.getElementById("volume");
+
+const clickedPlayBtn = (e) => {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+};
+
+const clickedMuteBtn = (e) => {};
+
+const playedvideo = (e) => {
+  playBtn.innerText = "Pause";
+};
+
+const pausedVideo = (e) => {
+  playBtn.innerText = "Play";
+};
+playBtn.addEventListener("click", clickedPlayBtn);
+muteBtn.addEventListener("click", clickedMuteBtn);
+video.addEventListener("played", playedVideo);
+video.addEventListener("pause", pausedVideo);
