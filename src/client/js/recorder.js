30afc717e1da67b3db9d52ba526fc0893427e00d
L2HYUNN <dhl9810@naver.com>
2022-03-20 19:52:02 +0900

    #14.2 Download Transcoded Video

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -16,9 +16,17 @@ const videoDownload = async () => {
 
   await ffmpeg.run("-i", "recording.webm", "-r", "60", "output.mp4");
 
+  const mp4File = ffmpeg.FS("readFile", "output.mp4");
+
+  console.log(mp4File);
+
+  const mp4Blob = new Blob([mp4File.buffer], { type: "video/mp4" });
+
+  const mp4Url = URL.createObjectURL(mp4Blob);
+
   const a = document.createElement("a");
-  a.href = videoFile;
-  a.download = "VideoFile.webm";
+  a.href = mp4Url;
+  a.download = "VideoFile.mp4";
   document.body.appendChild(a);
   a.click();
 };
