6a76a4a68a546c06f4d255d4009ebcbcf40214dc
L2HYUNN <dhl9810@naver.com>
2022-03-20 22:08:34 +0900

    #14.5 Thumbnail Upload part One

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,33 +1,51 @@
 import { createFFmpeg, fetchFile } from "@ffmpeg/ffmpeg";
 
-const startBtn = document.getElementById("startBtn");
+const actionBtn = document.getElementById("actionBtn");
 const video = document.getElementById("preview");
 
 let stream, recorder, videoFile;
 
+const files = {
+  input: "recording.webm",
+  output: "output.mp4",
+  thumb: "thumbnail.jpg",
+};
+
+const downloadFile = (fileUrl, fileName) => {
+  const a = document.createElement("a");
+  a.href = fileUrl;
+  a.download = fileName;
+  document.body.appendChild(a);
+  a.click();
+};
+
 const videoDownload = async () => {
+  actionBtn.removeEventListener("click", videoDownload);
+  actionBtn.innerText = "Transcording...";
+  actionBtn.disabled = true;
+
   const ffmpeg = createFFmpeg({
     corePath: "/convert/ffmpeg-core.js",
     log: true,
   });
   await ffmpeg.load();
 
-  ffmpeg.FS("writeFile", "recording.webm", await fetchFile(videoFile));
+  ffmpeg.FS("writeFile", files.input, await fetchFile(videoFile));
 
-  await ffmpeg.run("-i", "recording.webm", "-r", "60", "output.mp4");
+  await ffmpeg.run("-i", files.input, "-r", "60", files.output);
 
   await ffmpeg.run(
     "-i",
-    "recording.webm",
+    files.input,
     "-ss",
     "00:00:01",
     "-frames:v",
     "1",
-    "thumbnail.jpg"
+    files.thumb
   );
 
-  const mp4File = ffmpeg.FS("readFile", "output.mp4");
-  const thumbFile = ffmpeg.FS("readFile", "thumbnail.jpg");
+  const mp4File = ffmpeg.FS("readFile", files.output);
+  const thumbFile = ffmpeg.FS("readFile", files.thumb);
 
   const mp4Blob = new Blob([mp4File.buffer], { type: "video/mp4" });
   const thumbBlob = new Blob([thumbFile.buffer], { type: "image/jpg" });
@@ -35,38 +53,33 @@ const videoDownload = async () => {
   const mp4Url = URL.createObjectURL(mp4Blob);
   const thumbUrl = URL.createObjectURL(thumbBlob);
 
-  const mp4A = document.createElement("a");
-  mp4A.href = mp4Url;
-  mp4A.download = "videoFile.mp4";
-  document.body.appendChild(mp4A);
-  mp4A.click();
+  downloadFile(mp4Url, "MyRecording.mp4");
+  downloadFile(thumbUrl, "MyThumbnail.jpg");
 
-  const thumbA = document.createElement("a");
-  thumbA.href = thumbUrl;
-  thumbA.download = "thumbnail.jpg";
-  document.body.appendChild(thumbA);
-  thumbA.click();
-
-  ffmpeg.FS("unlink", "recording.webm");
-  ffmpeg.FS("unlink", "output.mp4");
-  ffmpeg.FS("unlink", "thumbnail.jpg");
+  ffmpeg.FS("unlink", files.input);
+  ffmpeg.FS("unlink", files.output);
+  ffmpeg.FS("unlink", files.thumb);
 
   URL.revokeObjectURL(mp4Url);
   URL.revokeObjectURL(thumbUrl);
   URL.revokeObjectURL(videoFile);
+
+  actionBtn.disabled = false;
+  actionBtn.innerText = "Record Again";
+  actionBtn.addEventListener("click", clickedRecordBtn);
 };
 
 const clickedStopRecordBtn = () => {
-  startBtn.innerText = "Download Recording";
-  startBtn.removeEventListener("click", clickedStopRecordBtn);
-  startBtn.addEventListener("click", videoDownload);
+  actionBtn.innerText = "Download Recording";
+  actionBtn.removeEventListener("click", clickedStopRecordBtn);
+  actionBtn.addEventListener("click", videoDownload);
   recorder.stop();
 };
 
 const clickedRecordBtn = () => {
-  startBtn.innerText = "Stop Recording";
-  startBtn.removeEventListener("click", clickedRecordBtn);
-  startBtn.addEventListener("click", clickedStopRecordBtn);
+  actionBtn.innerText = "Stop Recording";
+  actionBtn.removeEventListener("click", clickedRecordBtn);
+  actionBtn.addEventListener("click", clickedStopRecordBtn);
 
   recorder = new MediaRecorder(stream);
   recorder.ondataavailable = (event) => {
@@ -90,4 +103,4 @@ const init = async () => {
 
 init();
 
-startBtn.addEventListener("click", clickedRecordBtn);
+actionBtn.addEventListener("click", clickedRecordBtn);
--- src/models/Video.js
+++ src/models/Video.js
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 
 const videoSchema = new mongoose.Schema({
   videoUrl: { type: String, required: true },
+  thumbUrl: { type: String, required: true },
   title: { type: String, required: true, trim: true },
   description: { type: String, required: true, trim: true },
   createdAt: { type: Date, required: true, default: Date.now },
