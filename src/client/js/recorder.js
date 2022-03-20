c594bed40e6d476b06a68be2f34eee372ff90a82
L2HYUNN <dhl9810@naver.com>
2022-03-20 13:26:07 +0900

    #13.1 Video Preview

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,10 +1,13 @@
 const startBtn = document.getElementById("startBtn");
+const video = document.getElementById("preview");
 
 const clickedStartBtn = async () => {
   const stream = await navigator.mediaDevices.getUserMedia({
     audio: true,
     video: true,
   });
+  video.srcObject = stream;
+  video.play();
 };
 
 startBtn.addEventListener("click", clickedStartBtn);
