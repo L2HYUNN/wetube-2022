8825ec1cbbef4c97e89a373645a6d557ae35c24b
L2HYUNN <dhl9810@naver.com>
2022-03-20 13:57:56 +0900

    #13.2 Recording Video

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,13 +1,33 @@
 const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 
-const clickedStartBtn = async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({
-    audio: true,
+let stream, recorder;
+
+const clickedStopRecordBtn = () => {
+  startBtn.innerText = "Start Recording";
+  startBtn.removeEventListener("click", clickedStopRecordBtn);
+  startBtn.addEventListener("click", clickedRecordBtn);
+  recorder.stop();
+};
+
+const clickedRecordBtn = () => {
+  startBtn.innerText = "Stop Recording";
+  startBtn.removeEventListener("click", clickedRecordBtn);
+  startBtn.addEventListener("click", clickedStopRecordBtn);
+
+  recorder = new MediaRecorder(stream);
+  recorder.start();
+};
+
+const init = async () => {
+  stream = await navigator.mediaDevices.getUserMedia({
+    audio: false,
     video: true,
   });
   video.srcObject = stream;
   video.play();
 };
 
-startBtn.addEventListener("click", clickedStartBtn);
+// init();
+
+startBtn.addEventListener("click", clickedRecordBtn);
