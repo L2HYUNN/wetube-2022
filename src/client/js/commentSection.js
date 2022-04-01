e707350df5aa788ed947e5b73b9f394278b4622f
L2HYUNN <dhl9810@naver.com>
2022-04-01 21:17:19 +0900

    #16.3 API Route part One

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,12 +1,21 @@
+const { default: fetch } = require("node-fetch");
+
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
-const textarea = form.querySelector("textarea");
-const btn = form.querySelector("button");
 
 const handleCommentSubmit = (event) => {
   event.preventDefault();
+  const textarea = form.querySelector("textarea");
   const text = textarea.value;
-  const video = videoContainer.dataset.id;
+  const videoId = videoContainer.dataset.id;
+  fetch(`/api/videos/${videoId}/comment`, {
+    method: "POST",
+    body: {
+      text,
+    },
+  });
 };
 
-form.addEventListener("submit", handleCommentSubmit);
+if (form) {
+  form.addEventListener("submit", handleCommentSubmit);
+}
