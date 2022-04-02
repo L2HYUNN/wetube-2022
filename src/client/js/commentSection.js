2de20fa8b74c4bfd9706f21fb9dffc1c9500e12a
L2HYUNN <dhl9810@naver.com>
2022-04-02 10:45:25 +0900

    #16.7 Realtime Comments

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -3,6 +3,19 @@ const { default: fetch } = require("node-fetch");
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
 
+const addComment = (text) => {
+  const videoComments = document.querySelector(".video__comments ul");
+  const newComment = document.createElement("li");
+  newComment.className = "video__comment";
+  const icon = document.createElement("i");
+  icon.className = "fas fa-comment";
+  const span = document.createElement("span");
+  span.innerText = ` ${text}`;
+  newComment.appendChild(icon);
+  newComment.appendChild(span);
+  videoComments.prepend(newComment);
+};
+
 const handleCommentSubmit = async (event) => {
   event.preventDefault();
   const textarea = form.querySelector("textarea");
@@ -11,7 +24,7 @@ const handleCommentSubmit = async (event) => {
   if (text === "") {
     return;
   }
-  await fetch(`/api/videos/${videoId}/comment`, {
+  const { status } = await fetch(`/api/videos/${videoId}/comment`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -21,6 +34,9 @@ const handleCommentSubmit = async (event) => {
     }),
   });
   textarea.value = "";
+  if (status === 201) {
+    addComment(text);
+  }
   window.location.reload();
 };
 
